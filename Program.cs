23a978f0794c58e818b7cca4ff4b692a96896062
ss1s1 <127394594+ss1s1@users.noreply.github.com>
2024-12-13 04:26:59 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ namespace Fashion_Company
             });
             
             var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL")
-                ?? builder.Configuration.GetConnectionString("DefaultConnection");;
+                ?? builder.Configuration.GetConnectionString("DefaultConnection");
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(connectionString));
