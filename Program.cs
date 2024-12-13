d8703249afc2d85622781e1db61b717d28a8706e
ss1s1 <127394594+ss1s1@users.noreply.github.com>
2024-12-13 11:47:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -30,8 +30,7 @@ namespace Fashion_Company
             });
 
             // Добавляем службу для Entity Framework Core
-            var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL")
-                ?? builder.Configuration.GetConnectionString("DefaultConnection");
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(connectionString));
